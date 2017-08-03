5d5a408c7ba902e28a897ab375cf4d962133fb77
Mohammed alsokait <malsokait@gmail.com>
2017-08-03 08:41:24 +0000

    done

--- server.js
+++ server.js
@@ -1,84 +1,21 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+const express = require("express");
+const app = express();
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+app.get('/api/whoami', function(req, res){
+  const language = req.headers['accept-language'].toString().slice(0,5);
+  const softwareString = req.headers['user-agent'].toString();
+  const software = softwareString.slice(softwareString.search(/[(]/g)+1, softwareString.search(/[)]/g));
+  const ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const jsonResponse = {'ipaddress': ipaddress,
+                      'language': language,
+                      'software': software
+  };
+  res.json(jsonResponse);
+})
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+app.listen(8080, function(){
+  console.log("Running on 8080");
+  
+})
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
